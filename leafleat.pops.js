98d25fe4880a77b98efff8d00aa8dbcb494e7cfe
Sergey Molokovskikh <s.molokovskih@bftcom.com>
2015-04-02 14:55:33 +0700

    Опция crs для L.TileLayer вынесенна в хук

--- leafleat.pops.js
+++ leafleat.pops.js
@@ -12,7 +12,7 @@
         // Node/CommonJS
         module.exports = factory(require('leaflet'));
     } else {
-        // Проверка на Leafleat
+        // Browser globals
         if (typeof this.L === 'undefined')
             throw 'Leaflet must be loaded first!';
         // Namespace        
@@ -21,11 +21,86 @@
 }(function (nsTile) {
 
 
+//Add hook to class L.TileLayer
+nsTile.addInitHook(L.Util.extend(
+//Hook for CRS-option TileLayer
+function(){
+	
+	if(this.options.crs){		
+		var self = this,
+			objSync={},
+			fn_override=arguments.callee.fn_list||['_reset','_update'],
+			leftPointTestProps = arguments.callee.leftPoint_propNames||['_initialTopLeftPoint','_pixelOrigin'],
+			override,
+			f,fn_name;
+					
+		for(f in fn_override){
+			fn_name = fn_override[f];			
+			if(fn_name in self){
+				
+				override=function() {
+					
+					var context_fn = arguments.callee,
+						params = !!objSync.running?context_fn.caller.arguments:arguments;
+					
+					if(!!!objSync.running)
+					{
+						objSync.running=true;						
+						
+						var result,						
+							fn_map = function(a,b,c){
+								for(var i=0;i<b.length;i++)
+									if(b[i] in a)			
+										a[b[i]]=c(b[i],a[b[i]]);			
+							},
+								 m = self._map,
+								 o = m&&m.options,
+								 z = m&&m.getZoom(),
+								 new_crs = self.options.crs,
+								 old_crs = o&&o.crs,
+								 old_po;
+									
+						if(m&&!(new_crs===old_crs)) {									  									
+								  old_po  = old_po||{};
+								  fn_map(m,leftPointTestProps,function(key,value){
+									 old_po[key]=value;
+									 return new_crs.latLngToPoint(old_crs.pointToLatLng(value,z),z).round();				 
+								  });
+								  o.crs = new_crs;							
+						}
+						 
+						result = context_fn.original.apply(self,params);
+						
+						if(old_po){
+							fn_map(m,leftPointTestProps,function(key,value){ return old_po[key]; });
+							o.crs = old_crs;
+						}							
+						
+						objSync.running = false;
+						return result;
+					}
+					
+					return context_fn.original.apply(self,params);
+				};				
+				override.original=self[fn_name];
+				self[fn_name]=override;
+			}
+		}		
+	}
+},
+{
+	//Set list override function
+	fn_list : ['_reset','_update'],
+	//Set list leftTopPoint internal names
+	leftPoint_propNames: ['_initialTopLeftPoint','_pixelOrigin']
+})
+),
+
+
 //2GIS
 nsTile.DGis = nsTile.extend({
    statics:{ create :function (url,options) {return new nsTile.DGis(url,options) }},  
-   initialize: function(url,options)
-	{
+   initialize: function(url,options){
 		 var sd = {subdomains:[0,1,2,3,4,5]};		 
 		 return nsTile.prototype.initialize.call(this,url||'http://tile{s}.maps.2gis.ru/tiles?x={x}&y={y}&z={z}',(options&&L.Util.extend({},sd,options))||sd);
     }
@@ -34,72 +109,16 @@ nsTile.DGis = nsTile.extend({
 //Yandex
 nsTile.Yandex = nsTile.extend({
    statics:{ create :function (url,options) {return new nsTile.Yandex(url,options);}},  
-   initialize: function(url,options)
-	{
-		var sd = {subdomains:['00','01','02','03','04']};
+   initialize: function(url,options){
+		var sd = {crs:L.CRS.EPSG3395,subdomains:['00','01','02','03','04']};		
 		return nsTile.prototype.initialize.call(this,url||'http://{s}.pvec.maps.yandex.net/?l=pmap&x={x}&y={y}&z={z}',(options&&L.Util.extend({},sd,options))||sd);		 
-    },
-	_overrideFn:function(name,params)
-	{				 
-		var leftPointTestProps = ['_initialTopLeftPoint','_pixelOrigin'],
-			protoTile  = nsTile.prototype;
-				
-		 if(!(name in protoTile))
-			 return;
-		 
-		 
-		var fn_map = function(a,b,c)
-		{
-			for(var i=0;i<b.length;i++)
-				if(b[i] in a)			
-					a[b[i]]=c(b[i],a[b[i]]);			
-		};
-		 
-		 var fn = protoTile[name],
-			 m = this._map,
-			 o = m&&m.options,
-			 z = m&&m.getZoom(),
-			 yandexCrs = L.CRS.EPSG3395,
-			 old_crs = o&&o.crs,
-			 old_po  = {};
-				
-		if(m) 
-			  fn_map(m,leftPointTestProps,function(key,value){
-				 old_po[key]=value;
-				 return yandexCrs.latLngToPoint(old_crs.pointToLatLng(value,z),z).round();				 
-			  });
-			  
-		//if(m) m._pixelOrigin = yandexCrs.latLngToPoint(old_crs.pointToLatLng(m._pixelOrigin,z),z).round();		
-		
-		if(o) o.crs = yandexCrs;
-		 
-		var r = fn&&fn.apply(this,params||arguments.callee.caller.arguments);
-		
-		
-		if(m) 
-			fn_map(m,leftPointTestProps,function(key,value){ return old_po[key]; });
-		
-		//if(m) m._pixelOrigin = old_po;
-		if(o) o.crs = old_crs;
-		
-		return r;
-	},
-	_reset:function()
-	{
-		return this._overrideFn('_reset');		
-	},
-	
-	_update:function()
-	{
-		return this._overrideFn('_update');		
-	}
+    }
 }),
 
 //OpenStreetMap 
 nsTile.Osm = nsTile.extend({
    statics:{ create :function (url,options) {return new nsTile.Osm(url,options);}},  
-   initialize: function(url,options)
-	{
+   initialize: function(url,options){
 		 return nsTile.prototype.initialize.call(this,url||'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',options);
     }
 }),
@@ -107,8 +126,7 @@ nsTile.Osm = nsTile.extend({
 //Google
 nsTile.Google = nsTile.extend({
    statics:{ create :function (url,options) {return new nsTile.Google(url,options);}},  
-   initialize: function(url,options)
-	{
+   initialize: function(url,options){
 		 return nsTile.prototype.initialize.call(this,url||'http://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}',options);
     }
 })
